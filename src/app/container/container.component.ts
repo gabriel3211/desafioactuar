942d8420e83f7f0d3549abf2948c14e6b22d55a1
Gabriel Guimarães <gabriel3211@gmail.com>
2020-11-12 00:44:52 -0300

    Feito alinhamento CSS via Flex

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ObterAlunosService } from '../services/obter-alunos.service'
-import { Alunos } from '../services/services.interface'
+import { ObterAlunosService } from '../services/obter-alunos.service';
+import { Alunos } from '../services/services.interface';
+import { LocalStorageService } from '../services/local-storage-service.service';
 
 @Component({
   selector: 'app-container',
@@ -11,22 +12,27 @@ export class ContainerComponent implements OnInit {
   
   
   loading: boolean = true;
-  public alunos: Alunos[];
+  private alunos: Alunos[];
+  public alunoslocal: Alunos[];
   
-  constructor( protected http: ObterAlunosService) { }
+  
+  constructor( protected http: ObterAlunosService, protected localstorage: LocalStorageService) { }
 
- 
+  obterAlunosLocal(){
+    this.alunoslocal = JSON.parse(this.localstorage.get("alunoslocal"))
+  }
 
   ngOnInit(): void {  
 
     this.http.obterAlunos().subscribe(response => {
       
       this.alunos = response;
+      this.localstorage.set("alunoslocal", JSON.stringify(this.alunos))
+      this.obterAlunosLocal();
       this.loading = false;
-      console.log(this.alunos)
-     
-    })
-
+      console.log(this.alunoslocal)
+    });
+    
   }
 
 }
