2dfe6b99cba0c5a16dd675497de9fa0e0ea8e614
Gabriel Guimarães <gabriel3211@gmail.com>
2020-11-14 11:58:36 -0300

    toques finais

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -12,14 +12,20 @@ export class ContainerComponent implements OnInit {
   
   
   loading: boolean = true;
-  divalunos: boolean = false;
-  divcadastro: boolean = true;
+  divalunos: boolean = true;
+  cadastrar: boolean = false;
+  alterarbtn: boolean = false;
+  divcadastro: boolean = false;
+  checkfeminino: boolean = false;
+  checkmasculino: boolean = false;
+  atualindex: number;
+  titulo: string;
   private alunos: Alunos[];
   public alunoslocal: Alunos[];
-  public nome: string;
-  public email: string;
-  public dtnascimento: string;
-  public sexo: string;
+  public nome: string = "";
+  public email: string = "";
+  public dtnascimento: string = "";
+  public sexo: string = "";
 
 
 
@@ -29,13 +35,48 @@ export class ContainerComponent implements OnInit {
     this.alunoslocal = JSON.parse(this.localstorage.get("local"));
 
   }
+  adicionarItem(){
+    if(this.divalunos){
+      this.divalunos = false;
+      this.divcadastro = true;
+      this.titulo = "Novo cadastro";
+  }
+  this.cadastrar = true;
+  this.alterarbtn = false;
+  this.nome = "";
+  this.sexo = "";
+  this.email = "";
+  this.dtnascimento = "";
+  };
   voltar(){
     if(this.divcadastro){
       this.divalunos = true;
       this.divcadastro = false;
     }
- 
   }
+
+  alterarItem(event: { target: { value: number; }; }){
+    let index = event.target.value;
+    this.atualindex = index;
+    this.cadastrar = false;
+    this.alterarbtn = true;
+    if(this.divalunos){
+      this.divalunos = false;
+      this.divcadastro = true;
+      this.titulo = "Alterar Cadastro";
+    }
+    this.nome = this.alunoslocal[index].Nome;
+    this.email = this.alunoslocal[index].Email;
+    this.dtnascimento = this.alunoslocal[index].DataNascimento;
+    this.sexo = this.alunoslocal[index].Sexo;
+    if(this.sexo === 'Feminino'){
+      this.checkfeminino = true;
+    }
+    if(this.sexo === 'Masculino'){
+      this.checkmasculino = true;
+    }
+  }
+
   excluirItem(event: { target: { value: number; }; }) { 
     let index = event.target.value;
     this.alunoslocal.splice(index, 1);
@@ -60,6 +101,16 @@ export class ContainerComponent implements OnInit {
     DataNascimento: this.dtnascimento,
     Sexo: this.sexo
     });
+    this.localstorage.set("local", JSON.stringify(this.alunoslocal));
+    this.obterAlunosLocal();
+    this.voltar();
+  }
+  alterar(){
+    
+    this.alunoslocal[this.atualindex].Nome = this.nome;
+    this.alunoslocal[this.atualindex].Email = this.email;
+    this.alunoslocal[this.atualindex].Sexo = this.sexo;
+    this.alunoslocal[this.atualindex].DataNascimento = this.dtnascimento;
     this.localstorage.set("local", JSON.stringify(this.alunoslocal));
     this.obterAlunosLocal();
     this.voltar();
